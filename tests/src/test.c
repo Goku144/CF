3379fccce0b34b3ed64cd56b37992f74e0d838ea
Goku144 <ytyui31@gmail.com>
2026-04-09 21:16:54 +0100

    checking test faild work

--- tests/src/test.c
+++ tests/src/test.c
@@ -2,7 +2,7 @@
 
 CF_STATIC_ASSERT(sizeof(cf_u8) == 1, "cf_u8 must be 1 byte");
 
-
+CF_STATIC_ASSERT(sizeof(cf_u8) == 2, "cf_u8 test failed");
 /* Uncomment to force failure */
 /* CF_STATIC_ASSERT(sizeof(cf_u8) == 2, "cf_u8 test failed"); */
 
