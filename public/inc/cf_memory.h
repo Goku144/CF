f5f09e4ef9af640b1796817911d59e50744ebceb
Goku144 <ytyui31@gmail.com>
2026-04-10 01:13:26 +0100

    Adding empty from function helpers

--- public/inc/cf_memory.h
+++ public/inc/cf_memory.h
@@ -39,4 +39,29 @@ typedef struct cf_buffer
   cf_usize cap;
 } cf_buffer;
 
+/*
+* Return Initialized cf_bytes as (CF_NULL,0)
+*/
+cf_bytes cf_bytes_empty(void);
+
+/*
+* Return Initialized cf_bytes_mut as (CF_NULL,0)
+*/
+cf_bytes_mut cf_bytes_mut_empty(void);
+
+/*
+* Return Initialized cf_buffer as (CF_NULL,0,0)
+*/
+cf_buffer cf_buffer_empty(void);
+
+/*
+* Return Already existing memory data as cf_bytes for read-only data
+*/
+cf_bytes cf_bytes_from(const cf_u8 *data, cf_usize len);
+
+/*
+* Return Already existing memory data as cf_bytes_mut for writable data
+*/
+cf_bytes_mut cf_bytes_mut_from(cf_u8 *data, cf_usize len);
+
 #endif // CF_MEMORY_H
--- public/inc/cf_string.h
+++ public/inc/cf_string.h
@@ -30,4 +30,19 @@ typedef struct cf_string
     cf_usize cap;
 } cf_string;
 
+/*
+* Return Initialized cf_str as (CF_NULL,0)
+*/
+cf_str cf_str_empty(void);
+
+/*
+* Return Initialized cf_string as (CF_NULL,0,0)
+*/
+cf_string cf_string_empty(void);
+
+/*
+* Return Already existing memory data as cf_str for writable data
+*/
+cf_str cf_str_from(const char *data, cf_usize len);
+
 #endif // CF_STRING_H
