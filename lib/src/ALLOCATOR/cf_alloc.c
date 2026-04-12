b68f166deae480919fb9698845dcbba70dc87c43
Goku144 <ytyui31@gmail.com>
2026-04-12 20:45:33 +0100

    adding allocator struture

--- lib/src/ALLOCATOR/cf_alloc.c
+++ lib/src/ALLOCATOR/cf_alloc.c
@@ -1,4 +1,9 @@
 #include "ALLOCATOR/cf_alloc.h"
+#include "RUNTIME/cf_types.h"
 
-/* suppress ISO C empty translation unit warning */
-typedef int cf_alloc_placeholder;
+typedef struct cf_allocator
+{
+  void *(*alloc) (void *ctx, cf_usize size);
+  void *(*realloc) (void *ctx, void *ptr, cf_usize new_size);
+  void  (*free) (void *ctx, void *ptr);
+}cf_allocator;
