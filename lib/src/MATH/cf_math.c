907884b42d372f2637c973cc64bde1afeca831db
Goku144 <ytyui31@gmail.com>
2026-04-25 15:42:49 +0100

    add helper math functions

--- lib/src/MATH/cf_math.c
+++ lib/src/MATH/cf_math.c
@@ -30,3 +30,37 @@ cf_u8 cf_math_g8_mul_mod(cf_u8 p, cf_u8 q)
   }while (q >>= 1);
   return res;
 }
+
+cf_u8 cf_math_rotl8(cf_u8 x, cf_u8 n)
+{
+  n &= 7;
+  return n == 0 ? x : (cf_u8)((x << n) | (x >> (8 - n)));
+}
+
+cf_u8 cf_math_rotr8(cf_u8 x, cf_u8 n)
+{
+  n &= 7;
+  return n == 0 ? x : (cf_u8)((x >> n) | (x << (8 - n)));
+}
+
+cf_u32 cf_math_rotl32(cf_u32 x, cf_u8 n)
+{
+  n &= 31;
+  return n == 0 ? x : (x << n) | (x >> (32 - n));
+}
+
+cf_u32 cf_math_rotr32(cf_u32 x, cf_u8 n)
+{
+  n &= 31;
+  return n == 0 ? x : (x >> n) | (x << (32 - n));
+}
+
+cf_usize cf_math_min_usize(cf_usize a, cf_usize b)
+{
+  return a <= b ? a : b;
+}
+
+cf_usize cf_math_max_usize(cf_usize a, cf_usize b)
+{
+  return a >= b ? a : b;
+}
--- public/inc/MATH/cf_math.h
+++ public/inc/MATH/cf_math.h
@@ -33,4 +33,70 @@
  */
 cf_u8 cf_math_g8_mul_mod(cf_u8 p, cf_u8 q);
 
+/**
+ * @brief Rotate an 8-bit value left.
+ *
+ * The rotation count is reduced modulo 8, so values greater than 7 wrap
+ * naturally.
+ *
+ * @param x Value to rotate.
+ * @param n Bit count to rotate by.
+ * @return Rotated 8-bit value.
+ */
+cf_u8 cf_math_rotl8(cf_u8 x, cf_u8 n);
+
+/**
+ * @brief Rotate an 8-bit value right.
+ *
+ * The rotation count is reduced modulo 8, so values greater than 7 wrap
+ * naturally.
+ *
+ * @param x Value to rotate.
+ * @param n Bit count to rotate by.
+ * @return Rotated 8-bit value.
+ */
+cf_u8 cf_math_rotr8(cf_u8 x, cf_u8 n);
+
+/**
+ * @brief Rotate a 32-bit value left.
+ *
+ * The rotation count is reduced modulo 32, so values greater than 31 wrap
+ * naturally.
+ *
+ * @param x Value to rotate.
+ * @param n Bit count to rotate by.
+ * @return Rotated 32-bit value.
+ */
+cf_u32 cf_math_rotl32(cf_u32 x, cf_u8 n);
+
+/**
+ * @brief Rotate a 32-bit value right.
+ *
+ * The rotation count is reduced modulo 32, so values greater than 31 wrap
+ * naturally.
+ *
+ * @param x Value to rotate.
+ * @param n Bit count to rotate by.
+ * @return Rotated 32-bit value.
+ */
+cf_u32 cf_math_rotr32(cf_u32 x, cf_u8 n);
+
+/**
+ * @brief Return the smaller of two `cf_usize` values.
+ *
+ * @param a First value.
+ * @param b Second value.
+ * @return `a` when `a <= b`, otherwise `b`.
+ */
+cf_usize cf_math_min_usize(cf_usize a, cf_usize b);
+
+/**
+ * @brief Return the larger of two `cf_usize` values.
+ *
+ * @param a First value.
+ * @param b Second value.
+ * @return `a` when `a >= b`, otherwise `b`.
+ */
+cf_usize cf_math_max_usize(cf_usize a, cf_usize b);
+
 #endif /* CF_MATH_H */
