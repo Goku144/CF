6d08fad7a16fddb5c45ae820e2ec84fac0d04e23
Goku144 <ytyui31@gmail.com>
2026-05-02 23:10:31 +0100

    adding misiing math functions

--- lib/src/MATH/cf_math.cu
+++ lib/src/MATH/cf_math.cu
@@ -18,6 +18,52 @@
 
 #include "MATH/cf_math.h"
 
+cf_u8 cf_math_g8_mul_mod(cf_u8 p, cf_u8 q)
+{
+  cf_u8 res = 0;
+  do
+  {
+    if(q & 0x01) res ^= p;
+    if(p & 0x80) p = (p << 1) ^ 0x1B;
+    else p <<= 1;
+  } while(q >>= 1);
+  return res;
+}
+
+cf_u8 cf_math_rotl8(cf_u8 x, cf_u8 n)
+{
+  n &= 7;
+  return n == 0 ? x : (cf_u8)((x << n) | (x >> (8 - n)));
+}
+
+cf_u8 cf_math_rotr8(cf_u8 x, cf_u8 n)
+{
+  n &= 7;
+  return n == 0 ? x : (cf_u8)((x >> n) | (x << (8 - n)));
+}
+
+cf_u32 cf_math_rotl32(cf_u32 x, cf_u8 n)
+{
+  n &= 31;
+  return n == 0 ? x : (x << n) | (x >> (32 - n));
+}
+
+cf_u32 cf_math_rotr32(cf_u32 x, cf_u8 n)
+{
+  n &= 31;
+  return n == 0 ? x : (x >> n) | (x << (32 - n));
+}
+
+cf_usize cf_math_min_usize(cf_usize a, cf_usize b)
+{
+  return a <= b ? a : b;
+}
+
+cf_usize cf_math_max_usize(cf_usize a, cf_usize b)
+{
+  return a >= b ? a : b;
+}
+
 static cf_usize cf_math_type_size(cf_math_dtype dtype)
 {
   switch (dtype)
